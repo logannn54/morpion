e6200b9c47e1433375cc30af6097f3dc10b4fe66
Chapellier <logan@iutnc-503-07.iutnc.site.univ-lorraine.fr>
2024-05-23 17:13:27 +0200

    post fonctionnel

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+    res.render('index');
 });
 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
