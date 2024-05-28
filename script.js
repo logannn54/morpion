69add49ec34ba532e0df917c27cea9f5462cdfa1
Chapellier <logan@iutnc-503-07.iutnc.site.univ-lorraine.fr>
2024-05-28 10:42:56 +0200

    ajout table

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ const pool = new Pool({
     port: 5432,
 });
 
+const createTableQuery = `
+CREATE TABLE IF NOT EXISTS games (
+    id SERIAL PRIMARY KEY,
+    state TEXT,
+    player TEXT
+);
+`;
+
+pool.query(createTableQuery, (err, result) => {
+    if (err) {
+        console.error('Erreur lors de la création de la table : ', err);
+    } else {
+        console.log('La table games a été créée avec succès !');
+    }
+});
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
