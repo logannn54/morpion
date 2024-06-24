a63b3925d03ba28a908c95051c4b7f48326bc095
logannn54 <logan.chapellier3@etu.univ-lorraine.fr>
2024-06-23 20:07:35 -0500

    suppression fichier doublons

--- views/Square.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
